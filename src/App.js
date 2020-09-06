e3911d01b5218ab4cf4fefdaabb18d70044bb976
newt0 <py_lambda@yahoo.co.jp>
2020-09-06 22:38:45 +0900

    add: display compnents

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+import Widgets from "./Widgets";
 
 function App() {
   return (
-    // BEM
     <div className="app">
       <h1>Twitter Clone </h1>
 
-      {/* Sidebar */}
       <Sidebar />
 
-      {/* Feed */}
+      <Feed />
 
-      {/* Widgets */}
+      <Widgets />
     </div>
   );
 }
