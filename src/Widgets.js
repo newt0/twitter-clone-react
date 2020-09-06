b951c23e26b2915a931f4e568d1ed48fcf64f38e
newt0 <py_lambda@yahoo.co.jp>
2020-09-06 18:40:20 +0900

    add: Widgets

---
+++ src/Widgets.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Widgets.css";
+import SearchIcon from "@material-ui/icons/Search";
+
+function Widgets() {
+  return (
+    <div>
+      <h2>Widgets</h2>
+    </div>
+  );
+}
+
+export default Widgets;
