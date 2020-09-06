19c0f3ef3dbdcb7c9fd4d269db18c3f6068a5782
newt0 <py_lambda@yahoo.co.jp>
2020-09-07 00:46:24 +0900

    add: [WIP]forwardref and Flipmove

--- src/Feed.js
+++ src/Feed.js
@@ -3,6 +3,8 @@ import "./Feed.css";
 import TweetBox from "./TweetBox";
 import Post from "./Post";
 import db from "./firebase";
+import FlipMove from "react-flip-move";
+
 function Feed() {
   const [posts, setPosts] = useState([]);
 
@@ -19,17 +21,19 @@ function Feed() {
       </div>
 
       <TweetBox />
-
-      {posts.map((post) => (
-        <Post
-          displayName={post.displayName}
-          username={post.username}
-          verified={post.verified}
-          text={post.text}
-          avatar={post.avatar}
-          image={post.image}
-        />
-      ))}
+      <FlipMove>
+        {posts.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
+      </FlipMove>
     </div>
   );
 }
