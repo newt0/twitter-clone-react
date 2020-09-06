21b0a63bfdd65cea9eeb670e3fca090ac462595d
newt0 <py_lambda@yahoo.co.jp>
2020-09-06 18:40:05 +0900

    add: Feed

---
+++ src/Feed.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Feed.css";
+import TweetBox from "./TweetBox";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <h2>Home</h2>
+
+      <TweetBox />ƒ
+      
+    </div>
+  )
+}
+
+export default Feed
