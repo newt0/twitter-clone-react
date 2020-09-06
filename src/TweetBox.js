4162b2e68c13935c073e67cb7e347343a363d7ba
newt0 <py_lambda@yahoo.co.jp>
2020-09-07 00:30:22 +0900

    add: enable to post by hooks and firebase

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./TweetBox.css";
 import { Avatar, Button } from "@material-ui/core";
-
+import db from "./firebase";
 function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      displayName: "Newt999",
+      username: "newt999",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://images.unsplash.com/photo-1525672892528-eb2eead4effc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    });
+  };
+
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBox__input">
           <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
 
-          <input placeholder="What's happening?" type="text" />
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            className="tweetBox__imageInput"
+            placeholder="What's happening?"
+            type="text"
+          />
         </div>
+
         <input
-          className="tweetBox__imageInput"
-          placeholder="Optional: Enter image URL"
+          value={tweetImage}
+          onChange={(e) => setTweetImage(e.target.value)}
+          className="tweetBox imageInput"
           type="text"
+          placeholder="Optional: Enter image URL"
         />
-        <Button type="submit" className="tweetBox__tweetButton">
+        <Button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox__tweetButton"
+        >
           Tweet
         </Button>
       </form>
