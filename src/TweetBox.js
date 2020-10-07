cfad359ba60a922c05561788236781c46899a1f5
newt0 <py_lambda@yahoo.co.jp>
2020-10-07 18:48:41 +0900

    fix: avatar url

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -24,7 +24,7 @@ function TweetBox() {
     <div className="tweetBox">
       <form>
         <div className="tweetBox__input">
-          <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+          <Avatar src="https://pbs.twimg.com/profile_images/1222911891510398978/pKo4rhvz_400x400.jpg" />
 
           <input
             onChange={(e) => setTweetMessage(e.target.value)}
